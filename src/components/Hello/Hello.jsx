4cead8352ad150084a78875bf9a5e90c29aa58d3
zhangting <1092923594@qq.com>
2024-04-07 23:20:21 +0800

    update

---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import './Hello.css';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div className="title">Hello</div>
+    )
+  }
+}
